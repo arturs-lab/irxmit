e631303c81d11d1c45897053c98ab4c06b204ae6
arturj <mac_it_man@yahoo.com>
2025-06-01 01:08:14 -0500

    technics ir codes

---
+++ src/techn_ir.asm
@@ -0,0 +1,9 @@
+	org 0
+
+	db 002h,020h,0a0h,008h,095h,03dh	; deck 1/2
+
+	db 002h,020h,0a0h,008h,000h,0a8h	; deck_b_stop
+	db 002h,020h,0a0h,008h,00ah,0a2h	; deck_b_fwd
+	db 002h,020h,0a0h,008h,00bh,0a3h	; deck_b_rev
+	db 002h,020h,0a0h,008h,002h,0aah	; deck_b_ffwd
+	db 002h,020h,0a0h,008h,003h,0abh	; deck_b_frev
